88a03740bdf9aa17614f6436812524c2c22e71cb
Swaraj Zende <zendeswaraj53@gmail.com>
2024-04-06 17:26:55 +0530

    Added camera test code

---
+++ lane_detection/scripts/camera_test.py
@@ -0,0 +1,16 @@
+import cv2
+
+def main():
+    print('Camera_test')
+
+    cap = cv2.VideoCapture(15)
+    
+    while True:
+        ret,frame = cap.read()
+
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1)==ord('q'):
+            break
+
+if __name__ == '__main__':
+    main()
